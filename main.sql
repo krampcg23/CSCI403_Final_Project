bd746ec2c49688ac369d8dee7bcbd6623cec9b40
Allee Zarrini <azarrini@csm-wl-dhcp-204-93.mines.edu>
2018-11-18 12:25:05 -0700

    Import raw data from .csv without filtering

--- main.sql
+++ main.sql
@@ -6,8 +6,11 @@
 */
 
 DROP TABLE IF EXISTS tsunami CASCADE;
+DROP TABLE IF EXISTS tsunami_raw CASCADE;
 DROP TABLE IF EXISTS volcano CASCADE;
+DROP TABLE IF EXISTS volcano_raw CASCADE;
 DROP TABLE IF EXISTS earthquake CASCADE;
+DROP TABLE IF EXISTS earthquake_raw CASCADE;
 
 CREATE TABLE earthquake (
     date DATE,
@@ -16,7 +19,33 @@ CREATE TABLE earthquake (
     magnitude NUMERIC
 );
 
-\COPY earthquake (date, latitude, longitude, magnitude) FROM '~/Dropbox/Clayton/2018~2019/CSCI403/CSCI403_Final_Project/Data/earthquake-min.csv' DELIMITER ',' CSV;
+-- \COPY earthquake (date, latitude, longitude, magnitude) FROM '../../CSCI403_Final_Project/Data/earthquake-min.csv' DELIMITER ',' CSV;
+
+CREATE TABLE earthquake_raw (
+    date DATE,
+    time TEXT,
+    latitude NUMERIC(7,3),
+    longitude NUMERIC(7,3),
+    type TEXT,
+    depth NUMERIC,
+    depth_error NUMERIC,
+    depth_seismic NUMERIC,
+    magnitude NUMERIC NOT NULL,
+    magnitude_type TEXT,
+    magnitude_error NUMERIC,
+    magnitude_seismic NUMERIC,
+    azimuthal_gap NUMERIC,
+    horizontal_distance NUMERIC,
+    horizontal_error NUMERIC,
+    root_mean_square NUMERIC, 
+    ID TEXT,
+    source TEXT,
+    location TEXT,
+    magnitude_source TEXT,
+    status TEXT
+);
+
+\COPY earthquake_raw FROM '../../CSCI403_Final_Project/Data/earthquake.csv' DELIMITER ',' CSV;
 
 CREATE TABLE tsunami (
     year NUMERIC,
@@ -29,7 +58,7 @@ CREATE TABLE tsunami (
     intensity NUMERIC
 );
 
-\COPY tsunami FROM '~/Dropbox/Clayton/2018~2019/CSCI403/CSCI403_Final_Project/Data/tsunami-min.csv' DELIMITER ',' CSV;
+\COPY tsunami FROM '../../CSCI403_Final_Project/Data/tsunami-min.csv' DELIMITER ',' CSV;
 
 CREATE TABLE volcano (
     year NUMERIC,
@@ -40,4 +69,4 @@ CREATE TABLE volcano (
     longitude NUMERIC
 );
 
-\COPY volcano FROM '~/Dropbox/Clayton/2018~2019/CSCI403/CSCI403_Final_Project/Data/volcano-min.csv' DELIMITER ',' CSV;
+\COPY volcano FROM '../../CSCI403_Final_Project/Data/volcano-min.csv' DELIMITER ',' CSV;
