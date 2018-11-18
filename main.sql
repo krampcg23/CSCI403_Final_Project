f49e361ed1c6baaea06acc2290f54c1cb5ee560a
krampcg23 <krampcg23@gmail.com>
2018-11-18 11:04:47 -0700

    Importing data sets

--- main.sql
+++ main.sql
@@ -11,9 +11,33 @@ DROP TABLE IF EXISTS earthquake CASCADE;
 
 CREATE TABLE earthquake (
     date DATE,
-    latitude NUMERIC,
-    longitude NUMERIC,
+    latitude NUMERIC(7,3),
+    longitude NUMERIC(7,3),
     magnitude NUMERIC
 );
 
-\COPY earthquake (date, latitude, longitude, magnitude) FROM '~/Dropbox/Clayton/2018~2019/CSCI403/CSCI403_Final_Project/Data/earthquake.csv' WITH (FORMAT csv);
+\COPY earthquake (date, latitude, longitude, magnitude) FROM '~/Dropbox/Clayton/2018~2019/CSCI403/CSCI403_Final_Project/Data/earthquake-min.csv' DELIMITER ',' CSV;
+
+CREATE TABLE tsunami (
+    year NUMERIC,
+    month NUMERIC,
+    date NUMERIC,
+    magnitude NUMERIC,
+    country TEXT,
+    latitude NUMERIC,
+    longitude NUMERIC,
+    intensity NUMERIC
+);
+
+\COPY tsunami FROM '~/Dropbox/Clayton/2018~2019/CSCI403/CSCI403_Final_Project/Data/tsunami-min.csv' DELIMITER ',' CSV;
+
+CREATE TABLE volcano (
+    year NUMERIC,
+    month NUMERIC,
+    day NUMERIC,
+    country TEXT,
+    latitude NUMERIC,
+    longitude NUMERIC
+);
+
+\COPY volcano FROM '~/Dropbox/Clayton/2018~2019/CSCI403/CSCI403_Final_Project/Data/volcano-min.csv' DELIMITER ',' CSV;
